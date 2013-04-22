e6988343b19c8c0cf1e829b50fdc20886c4979de
Christian Conkle <christian@conkle.org>
2013-04-22 15:35:54 -0700

    Add applyMap and some basic benchmarks.  Results seem fine using either strict or lazy maps; the cost of applyMap seems about the same as the cost of id, i.e. most of the time is just evaluating the input data.  I guess that should have been obvious...

---
+++ benchmark/Benchmark.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE KindSignatures, DataKinds, TypeOperators, MultiParamTypeClasses, FlexibleContexts, OverloadedStrings #-}
+{-# LANGUAGE ConstraintKinds, RankNTypes #-}
+module Main where
+
+import Control.DeepSeq
+import Data.Vinyl
+import Control.Exception (evaluate)
+import Data.Map.Multidimensional
+import Criterion
+import Criterion.Main
+import Data.Map (Map)
+import qualified Data.Map as Map
+import System.Random
+
+--import Data.Text hiding (zip)
+
+type TestField1 = "one" ::: Int
+type TestField2 = "two" ::: Int
+testField1 :: TestField1
+testField2 :: TestField2
+testField1 = Field
+testField2 = Field
+--testRecord :: PlainRec '["one" ::: Int, "two" ::: Int]
+--testRecord = fixRecord $ testField1 =: "hello" <+> testField2 =: 3
+
+type MMC rs a = forall m. (MMap m rs) => m rs a 
+
+testHMap :: Int -> Int -> MMC  '[TestField1, TestField2] Int
+testHMap x y = fromList $  go x y
+  where go spaceSize itemsPerDimension = 
+            let keys = do
+                x' <- take itemsPerDimension $ randomRs (0,spaceSize) $ mkStdGen 1
+                y' <- take itemsPerDimension $ randomRs (0,spaceSize) $ mkStdGen 2
+                return (testField1 =: x' <+> testField2 =: y')
+            in keys `zip` [0..]
+            
+factorMap :: Int -> Int -> Map Int (Int -> Int)
+factorMap spaceSize items = force $
+    Map.fromList [(k,(+k))| k <- take items $ randomRs (0,spaceSize) $ mkStdGen 0]
+    
+nested :: Int -> Int -> NestedMMap '[TestField1, TestField2] Int
+nested n1 n2 = testHMap n1 n2
+
+main = do
+    input <- evaluate $ force $ nested 60 100
+    defaultMain
+        [bench "wh-id" $ whnf id input
+        ,bench "wh-notforced" $ whnf (applyMap testField1 (factorMap 60 100)) $ nested 60 100
+        ,bench "wh-forced" $ whnf (applyMap testField1 (factorMap 60 100)) $ input
+        ,bench "id" $ nf id input
+        ,bench "forced" $ nf (applyMap testField1 (factorMap 60 100)) $ input
+        ,bench "notforced" $ nf (applyMap testField1 (factorMap 60 100)) $ nested 60 100
+         
+        ]
+--    [bench "30 : 10*10  " $ nf (flip (applyMap testField1) $ nested 30 10) (factorMap 30 10) 
+--    ,bench "60 : 10*10  " $ nf (flip (applyMap testField1) $ nested 60 10) (factorMap 60 10) 
+--    ,bench "90 : 10*10  " $ nf (flip (applyMap testField1) $ nested 90 10) (factorMap 90 10)
+--    ,bench "30 : 10*10 " $ nf (flip (applyMap testField1) $ nested 30 10) (factorMap 30 10)
+--    ,bench "30 : 100*100 " $ nf (flip (applyMap testField1) $ nested 30 100) (factorMap 30 100)
+--    ,bench "30 : 1000*1000" $ nf (flip (applyMap testField1) $ nested 30 1000) (factorMap 30 1000)
+--    ,bench "1000 : 100*100" $ nf (flip (applyMap testField1) $ nested 1000 100) (factorMap 1000 100)
+--    ]
--- src/Data/Map/Multidimensional/Nested.hs
+++ src/Data/Map/Multidimensional/Nested.hs
@@ -6,29 +6,30 @@
 {-# LANGUAGE FlexibleInstances, OverlappingInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, RankNTypes #-}
 {-# LANGUAGE StandaloneDeriving, ScopedTypeVariables #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.Map.Multidimensional.Nested
     (NestedMMap
-    ,NestedMMapClass()
+    ,NestedMMapClass(applyMap)
     )
     where
 
 import Control.Applicative              ((<$>), Applicative, pure)
+import Control.DeepSeq
 import Data.Foldable                    (Foldable, foldMap)
 import Data.Functor.Identity            (runIdentity)
-import Data.Map                         (Map)
-import qualified Data.Map               as Map
+import Data.Map.Strict                    (Map)
+import qualified Data.Map.Strict               as Map
 import Data.Semigroup                   ((<>), mappend, mconcat, mempty, Monoid, Semigroup)
 import Data.Traversable                 (Traversable, traverse)
-import Data.Vinyl                       (Rec(..), (:::)(..), (=:), PlainRec)
+import Data.Vinyl                       --(Rec(..), (:::)(..), (=:), PlainRec)
 
 import Data.Map.Multidimensional.Class  (fromList, MMap, toList)
 
 newtype NestedMMap (rs :: [*]) v = NestedMMap { unNestedMMap :: (NMMapType rs v) } 
-    deriving (Semigroup, Monoid, Functor, Foldable, Traversable)
+    deriving (Semigroup, Monoid, Functor, Foldable, Traversable, NFData)
 
 class NestedMMapClass (rs :: [*]) where
     data NMMapType rs v :: *
@@ -40,8 +41,29 @@ class NestedMMapClass (rs :: [*]) where
     fmap' :: (a -> b) -> NMMapType rs a -> NMMapType rs b
     foldMap' :: Monoid m => (a -> m) -> NMMapType rs a -> m
     traverse' :: Applicative f => (a -> f b) -> NMMapType rs a -> f (NMMapType rs b)
+    rnf' :: NFData a => NMMapType rs a -> ()
+    applyMap :: forall thisSy t a b. IElem (thisSy ::: t) rs 
+             => (thisSy ::: t)
+             -> Map t (a -> b) 
+             -> NestedMMap rs a
+             -> NestedMMap rs b
+    applyMap s m = NestedMMap . applyMap' s m . unNestedMMap
+    {-# INLINE applyMap #-}
+    applyMap' :: forall thisSy t a b. IElem (thisSy ::: t) rs 
+              => (thisSy ::: t)
+              -> Map t (a -> b) 
+              -> NMMapType rs a 
+              -> NMMapType rs b
+    applyMap' = applyMap'' implicitly
+    {-# INLINE applyMap' #-}
+    applyMap'' :: Elem (thisSy ::: thisTy) rs 
+               => (thisSy ::: thisTy)
+               -> Map thisTy (a -> b) 
+               -> NMMapType rs a
+               -> NMMapType rs b
+               
     
-instance (Ord ty) => NestedMMapClass ((sy ::: ty) ': '[]) where
+instance (Ord ty, NFData ty) => NestedMMapClass ((sy ::: ty) ': '[]) where
     newtype NMMapType '[(sy ::: ty)] v = NMMBase (Map ty v)
     fromList' = NMMBase . Map.fromList . fmap go
       where
@@ -54,13 +76,22 @@ instance (Ord ty) => NestedMMapClass ((sy ::: ty) ': '[]) where
         go :: ty -> v -> [(PlainRec '[sy ::: ty], v)] 
         go t v = [(Field =: t, v)]
     {-# INLINE toList' #-}
+    applyMap'' e f fs (NMMBase m) = NMMBase $ Map.intersectionWith ($)
+         (Map.mapKeys (go e f) fs) m
+      where 
+        go :: Elem (thisSy ::: t) ((sy ::: ty) ': '[]) -> (thisSy ::: t) -> t -> ty
+        go Here _ = id
+        {-# INLINE go #-}
+    {-# INLINE applyMap'' #-}
+    rnf' (NMMBase m) = rnf m
+    {-# INLINE rnf' #-}
     fmap' f (NMMBase m) = NMMBase (fmap f m)
     mempty' = NMMBase $ Map.empty
     mappend' (NMMBase m1) (NMMBase m2) = NMMBase $  Map.unionWith (<>) m1 m2
     foldMap' f (NMMBase m) = foldMap f m
     traverse' f (NMMBase m) = NMMBase <$> traverse f m
 
-instance (Ord ty, NestedMMapClass (ks1 ': ks2)) => NestedMMapClass ((sy ::: ty) ': ks1 ': ks2) where
+instance (Ord ty, NFData ty, NestedMMapClass (ks1 ': ks2)) => NestedMMapClass ((sy ::: ty) ': ks1 ': ks2) where
     newtype NMMapType ((sy ::: ty) ': ks1 ': ks2) v = 
         NMMNest (Map ty (NMMapType (ks1 ': ks2) v)) 
     fromList' items = NMMNest $ (fmap fromList') $ Map.fromListWith (++) $ reKey <$> items
@@ -74,12 +105,16 @@ instance (Ord ty, NestedMMapClass (ks1 ': ks2)) => NestedMMapClass ((sy ::: ty) 
         go t m' = fmap (reKey t) $ toList' m' 
         reKey :: ty -> (PlainRec (ks1 ': ks2), v) -> (PlainRec ((sy ::: ty) ': ks1 ': ks2), v)
         reKey t (r, v) = (pure t :& r, v)
+    rnf' (NMMNest m) = rnf m
+    {-# INLINE rnf' #-}
     fmap' f (NMMNest m) = NMMNest $ fmap (fmap' f) m
     mempty' = NMMNest $ Map.empty
     mappend' (NMMNest m1) (NMMNest m2) = NMMNest $  Map.unionWith (<>) m1 m2
     foldMap' f (NMMNest m) = foldMap id $ foldMap' f <$> m
     traverse' f (NMMNest m) = NMMNest <$> traverse (traverse' f) m
-
+    applyMap'' Here _ fs (NMMNest m)= NMMNest $ Map.intersectionWith fmap fs m
+    applyMap'' (There e) f fs (NMMNest m)= NMMNest $ fmap (applyMap'' e f fs) m
+    {-# INLINE applyMap'' #-}
 instance (NestedMMapClass rs) => MMap NestedMMap rs where
     fromList = NestedMMap . fromList'
     toList = toList' . unNestedMMap   
@@ -90,9 +125,11 @@ instance (NestedMMapClass rs) => Traversable (NMMapType rs) where
     traverse = traverse'
 instance (NestedMMapClass rs) => Foldable (NMMapType rs) where
     foldMap = foldMap'
-instance (Semigroup v, NestedMMapClass rs) => Semigroup (NMMapType rs v) where
+instance (NestedMMapClass rs, NFData a) => NFData (NMMapType rs a) where
+    rnf = rnf'
+instance (Semigroup a, NestedMMapClass rs) => Semigroup (NMMapType rs a) where
     (<>) = mappend'
-instance (NestedMMapClass rs, Semigroup v) => Monoid (NMMapType rs v) where
+instance (NestedMMapClass rs, Semigroup a) => Monoid (NMMapType rs a) where
     mappend = (<>)
     mempty = mempty'
     
